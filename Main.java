af93e0f4b00f1637dc7642d093ba065192da29d0
Moksh Upadhyay <Mokshupadhyay57@github.com>
2021-03-13 07:06:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int i ;
+    System.out.println("You entered: ");
+    for(i =0;i<args.length;i++){
+        System.out.println(args[i]);
+    }
+    System.out.println("You entered " + i + " arguements");
+    
+  }
+}
